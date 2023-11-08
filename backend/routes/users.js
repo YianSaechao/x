fb738b4ac9f8ad960b4c2c77057ea33fd285a487
Yian Saechao <ysaechao@LABA.PSWAS.LOCAL>
2023-11-07 17:03:36 -0800

    bcryptjs error

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const UserModel = require('../models/Users.js');
 
 const router = express.Router();
